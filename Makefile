acfb93c9cf94c47df76509875d1d6e4c80b184a4
WatchYoJet <pedropereira972@gmail.com>
2021-06-22 03:42:00 +0100

    Add Parallel Execution System

---
+++ Makefile
@@ -0,0 +1,65 @@
+CONFIG=config.default
+include ${CONFIG}
+
+#---------Compiler-Options---------
+COMPILER=gcc
+CFLAGS=-Wall -Wextra -Werror -ansi -pedantic -g
+FILES=$(wildcard $(PROJECT_PATH)/*.c)
+#---------------Macros-------------
+# In order to change this, you'll need to do it on the making.sh file too
+HELP=-h
+MOVE=-m
+VALGRIND=-v
+LIZARD=-l
+#----------------------------------
+
+all:
+	$(COMPILER) $(CFLAGS) -o $(EXE_NAME) $(FILES)
+	mv $(EXE_NAME) $(PROJECT_PATH)
+
+run:
+	$(COMPILER) $(CFLAGS) -o $(EXE_NAME) $(FILES)
+	./$(EXE_NAME)
+
+m:
+	./making.sh $(MOVE) $(filter-out $@,$(MAKECMDGOALS))
+
+h:
+	./making.sh $(HELP)
+
+v:
+	touch v
+	$(MAKE) -j$(PROCESSOR) v
+	rm -f v
+	./making.sh $(VALGRIND)
+
+l:
+	touch l
+	$(MAKE) -j$(PROCESSOR) l
+	rm -f l
+	./making.sh $(LIZARD)
+
+testing:
+	touch test
+	$(MAKE) -j$(PROCESSOR) test
+	rm -f test
+	./making.sh
+
+#I hate this so much, trust me
+%:
+	@:
+
+difl:
+	diff -b ./tests/$(filter-out $@,$(MAKECMDGOALS)).out \
+	./tests/$(filter-out $@,$(MAKECMDGOALS)).myout \
+	> ./tests/$(filter-out $@,$(MAKECMDGOALS)).diff
+
+mof:
+	./$(EXE_NAME) < ./tests/$(filter-out $@,$(MAKECMDGOALS)).in \
+	> ./tests/$(filter-out $@,$(MAKECMDGOALS)).myout
+
+test_comp:
+	$(COMPILER) $(CFLAGS) -o $(EXE_NAME) $(FILES)
+
+clean:
+	rm -f $(PROJECT_PATH)/$(EXE_NAME) tests/*.diff tests/*.myout ./$(EXE_NAME) failed/*
--- UTest.sh
+++ UTest.sh
@@ -13,81 +13,91 @@ export red="\033[31;1m"
 export green="\033[32;1m"
 export reset="\033[0m"
 
-move_files(){
+move_files() {
     $(mv failed making.sh tests $1)
 }
 
-help(){
+help() {
     echo "TO-DO"
 }
 
-lizard_inst_check(){ ### Checks if Lizard is installed
+lizard_inst_check() { ### Checks if Lizard is installed
     if !(which lizard >/dev/null || (pip3 list | grep lizard >/dev/null)); then
         echo "Install Lizard First!"
         exit 1
     fi
 }
 
-
 ###  Using case statements to handle the flags
-while getopts ":m:hvl" opt; do 
+while getopts ":m:hvl" opt; do
     case $opt in
-    m)  ### Move file structure command
+    m) ### Move file structure command
         echo "[Moving] to $OPTARG..." >&2
         move_files $OPTARG
-        exit 1
+        exit 0
         ;;
-    h)  ### Help command
+    h) ### Help command
         help
-        exit 1
+        exit 0
         ;;
-    v)  ### Run the testing using Valgrind
-        echo "Valgrind" >&2
+    v) ### Run the testing using Valgrind
+        echo "Running With: Valgrind" >&2
         VALGRIND=1
         ;;
-    l)  ### Run the testing using Lizard
-        echo "Lizard" >&2
+    l) ### Run the testing using Lizard
+        echo "Running With: Lizard" >&2
         lizard_inst_check
         LIZARD=1
         ;;
     \?) ### Used invalid flag
         echo "Invalid option: -$OPTARG" >&2
-        exit 1
+        exit 0
         ;;
-    :)  ### The flag used requires a missing argument
+    :) ### The flag used requires a missing argument
         echo "Option -$OPTARG requires an argument." >&2
-        exit 1
+        exit 0
         ;;
     esac
 done
 
+make test_comp #Compiles the project for testing
 
+echo -e "\n\n----------------------------------"
+echo "Starting"
+echo "----------------------------------"
 
-exe_test(){
-
+passed=0
+total=0
+exe_test() { #Inspired by: Abread
     input=$1
-	test_name=$(basename $input | cut -f 1 -d'.')
-	expected=tests/$test_name.out
-	myout=failed/$name.myout
-    cat $input
+    test_name=$(basename $input | cut -f 1 -d'.')
+    expected=./tests/$test_name.out
+    myoutdir=./failed/$test_name.myout
     if ! [ -r $input ]; then
-		printf '%b' "$0: cannot read file "$input"\n${red}aborting$reset\n"
-		exit 1
-	fi
-	if ! [ -r $expected ]; then
-		printf '%b' "$0: cannot read file $expected\n${red}aborting$reset\n"
-		exit 1
-	fi
-	if make -j1 $myout > /dev/null && diff -b $expected $out &> /dev/null; then
-		printf '%b' "Test $test_name -- ${green}OK$reset\n"
-	else
-		printf '%b' "Test $test_name -- ${red}FAILED${reset}\n"
-		diff --color -b $expected $out
-		printf '%b' "$reset"
-		exit 1
-	fi
+        printf '%b' "$0: cannot read file "$input"\n${red}aborting$reset\n"
+        exit 1
+    fi
+    if ! [ -r $expected ]; then
+        printf '%b' "$0: cannot read file $expected\n${red}aborting$reset\n"
+        exit 1
+    fi
+    if make -j1 mof $test_name >/dev/null && make -j1 difl $test_name &>/dev/null; then
+        if [ "$(wc -l <failed/$test_name.diff)" -eq 0 ]; then
+            printf '%b' "Test $test_name -- ${green}OK$reset\n"
+            printf '%b' "$reset"
+            exit 0
+        fi
+    fi
+    cp ./tests/$test_name.myout ./failed
+    cp ./tests/$test_name.diff ./failed
+    printf '%b' "Test $test_name -- ${red}FAILED${reset}\n"
+    printf '%b' "$reset"
+    exit 0
 }
 
 export -f exe_test
 
 nice parallel --progress --eta --bar --joblog failed/logs $@ exe_test ::: ./tests/*.in
+
+total=$(cat failed/logs | tail -n +2 | wc -l)
+passed=$(cat failed/logs | tail -n +2 | cut -f7 | grep '0' | wc -l)
